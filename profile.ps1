4455bb649bff6016586f406c82cd3bf24a169eab
Devin Walker <devin.walker@sogeti.com>
2022-05-16 13:32:53 -0500

    Adding Context to script

--- profile.ps1
+++ profile.ps1
@@ -10,13 +10,14 @@
 # NOTE: any variables defined that are not environment variables will get reset after the first execution
 $global:subscription_id = "bbc94ccb-1e55-4b95-878c-c174172073f5"
 $global:tenant_id = "939f4a83-33ed-4845-a346-516f14ad6321"
-$global:StorageAccountName = "scratchareastorage"
+$global:StorageAccountName = $env:StorageAccountName
 #Login:
 Import-Module Az
 Connect-AzAccount -Subscription $subscription_id -Tenant $tenant_id -Identity
 $global:subscription = Get-AzSubscription -SubscriptionId $subscription_id -Tenant $tenant_id
 Set-AzContext -SubscriptionId $subscription_id
 Write-Host "Logged on to environment: "$subscription.Name -ForegroundColor "Cyan"
+$Context = New-AzStorageContext $StorageAccountName -StorageAccountKey "Lln8pswcwoZaMW8imspM+gDIK57X8QXKKa2mVziyZ1+lzdXxzpiFIqeenTUWE0yPYeEhXRTnkTde+ASteE+PWg=="
 
 # Authenticate with Azure PowerShell using MSI.
 # Remove this if you are not planning on using MSI or Azure PowerShell.
